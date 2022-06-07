6b438ec5ee5359913f6c65b7240dfe7c53a198a8
Ganeshp007 <potdart40.gp@gmail.com>
2022-06-07 23:20:32 +0530

    Added: Records Inserted into the employee_ payroll table

--- SQLQuery_For_Employee_Payroll_Service.sql
+++ SQLQuery_For_Employee_Payroll_Service.sql
@@ -1,15 +1,16 @@
 create database payroll_service                -- Creating database 
 select name from sys.databases                 -- Display all databases
 use Payroll_Service                            -- Selecting database to throw queries
-create table employee_payroll
+create table employee_payroll                  -- Creating table
 (
-  Id int NOT NULL Identity, 
+  Id int NOT NULL Identity,                    --declaring variables
   Name varchar(25) NOT NULL,
   start_date date
 )
 
-insert into employee_payroll values
+insert into employee_payroll values             -- INserting records in Table
 ('Ganesh Potdar','2022-08-11'),
-('Aman','2021-05-30')
+('Aman','2021-05-30'),
+('Mahesh','2015-06-28')
 
-select * from employee_payroll
+select * from employee_payroll                  -- Displaying record in table
