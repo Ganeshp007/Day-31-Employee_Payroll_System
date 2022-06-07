fa5af7cbf360cd75b3162145df83b5a478c04d34
Ganeshp007 <potdart40.gp@gmail.com>
2022-06-07 23:28:13 +0530

    Added: Records Inserted into the employee_ payroll table

--- SQLQuery_For_Employee_Payroll_Service.sql
+++ SQLQuery_For_Employee_Payroll_Service.sql
@@ -5,12 +5,13 @@ create table employee_payroll                  -- Creating table
 (
   Id int NOT NULL Identity,                    --declaring variables
   Name varchar(25) NOT NULL,
+  salary money,
   start_date date
 )
 
 insert into employee_payroll values             -- INserting records in Table
-('Ganesh Potdar','2022-08-11'),
-('Aman','2021-05-30'),
-('Mahesh','2015-06-28')
+('Ganesh Potdar','1000000','2022-08-11'),
+('Aman','800000','2021-05-30'),
+('Mahesh','750000','2015-06-28')
 
 select * from employee_payroll                  -- Displaying record in table
