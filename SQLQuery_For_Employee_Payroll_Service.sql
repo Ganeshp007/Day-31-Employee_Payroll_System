73715618262f32312826576dbadf2563514081dc
Ganeshp007 <potdart40.gp@gmail.com>
2022-06-09 16:09:31 +0530

    Added: Retrieveing Salary Records from employee_payroll Table

--- SQLQuery_For_Employee_Payroll_Service.sql
+++ SQLQuery_For_Employee_Payroll_Service.sql
@@ -9,9 +9,20 @@ create table employee_payroll                  -- Creating table
   start_date date
 )
 
+--UC3 => Inserting value in table
 insert into employee_payroll values             -- INserting records in Table
 ('Ganesh Potdar','1000000','2022-08-11'),
 ('Aman','800000','2021-05-30'),
-('Mahesh','750000','2015-06-28')
+('Mahesh','750000','2015-06-28'),
+('Dinesh','550000','2015-11-18'),
+('Komal','650000','2018-06-03'),
+('Abhishek','708000','2020-05-31')
 
-select * from employee_payroll                  -- Retriving all the record from table table and Display them
+--UC4 => Retrieveing all records from table
+select * from employee_payroll             -- Retriving all the record from table table
+
+--UC5 => Getting salary of particular employee
+select salary from employee_payroll where Name='Aman'   -- Display salaray of employee having name 'Aman'
+
+--Retrieve Record of salary from table on basis Start_date 
+select Name,salary from employee_payroll where start_date Between '2018-01-01' and GETDATE()
